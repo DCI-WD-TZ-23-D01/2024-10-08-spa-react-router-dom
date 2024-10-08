48f1625bf2b3cfcf3671d5efb7358f6e63aa971f
Marie Zirzlaff <marie.zirzlaff@dci-student.org>
2024-10-08 13:03:37 +0200

    Header

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,18 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Dashboard } from "../pages/Dashboard";
+import { Layout } from "../components/Layout";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-        
-        {
-          path: "/dashboard",
-          element: <Dashboard />,
-        },
-
-    ]
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
   },
 ]);
