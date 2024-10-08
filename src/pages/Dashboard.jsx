f918c88112acd627a42deae03aa3bc954a3cba9f
Lara <ioana-larisa.kempf@dci-student.org>
2024-10-08 11:48:04 +0200

    added links

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,11 @@
+import { NavLink } from "react-router-dom"
 export const Dashboard = () => {
-    return (
+    return (<>
         <h2>Hello Dashboard</h2>
-    )
+
+
+        <button> <NavLink to='/'>Link to Home</NavLink></button>
+
+    </>)
 
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import { NavLink } from "react-router-dom"
 export const Home = () => {
-    return (
+    return (<>
         <h2>Hello Home</h2>
-    )
+
+        <button> <NavLink to='dashboard'> Link to dashboard</NavLink></button>
+
+
+    </>)
 }
