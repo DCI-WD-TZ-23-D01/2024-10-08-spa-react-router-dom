aec8b98d4f1e9fcc4f1bd8840c4fc74eee0a9423
Nell <stephanie.konersmann@dci-student.org>
2024-10-08 11:39:32 +0200

    dashboard

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { Dashboard } from '../pages/Home';
 export const routes = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
   },
 ]);
