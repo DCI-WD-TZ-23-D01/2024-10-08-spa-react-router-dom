6938a95722e84d1fa9c5351179eb5d1a7705eccc
Natalia <khlipochenko@gmail.com>
2024-10-08 11:37:16 +0200

    add Dashboard

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,9 +1,13 @@
 import {createBrowserRouter} from "react-router-dom"
 import { Home } from "../pages/Home"
+import {  Dashboard } from "../pages/Dashboard"
 export const routers=createBrowserRouter([
     {
         element:<Home/>,
         path:'',
-        
+    },
+    {
+        element:<Dashboard></Dashboard>,
+        path:'dashboard'
     }
 ])
