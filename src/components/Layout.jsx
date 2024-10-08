ddd23c297345ee77188f08ecddd0a97d04bf9cac
Nell <stephanie.konersmann@dci-student.org>
2024-10-08 12:58:22 +0200

    weißnicht

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,25 @@
 export const Layout = () => {
   return (
     <>
-      <header>Header</header>
-      <Outlet />{' '}
+      <header>
+        <nav>
+          <h1>Nelloversum</h1>
+          <ul>
+            <li>
+              <a href="/">/Home</a>
+            </li>
+            <li>
+              <a href="/Dashboard">Dashboard</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />{' '}
+      </main>
+      <footer>
+        <p>&copy; 2024 - My Website:Nelloversum. All rights reserved</p>
+      </footer>
     </>
   );
 };
