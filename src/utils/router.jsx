7597b456f57058a1d235f041c72efd6922b50111
Zedan Ak <zedan.ak@dci-student.org>
2024-10-08 11:35:54 +0200

    Dashboard

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "../pages/Home";
+import { Dashboard } from "../pages/Dashboard";
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Home />,
     },
+
+    {
+        path: '/',
+        element: <Dashboard />,
+    }
 ]);
